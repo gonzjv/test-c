879ed34ecef79083b537fb90ebf7598bb9ff9f1a
gonzjv <gonzjv@ya.ru>
2023-04-01 09:19:29 +0300

    feat: find odd int task

--- findOddInt.c
+++ findOddInt.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 
 int find_odd(size_t length, const int array[length])
 {
@@ -21,15 +22,26 @@ int find_odd(size_t length, const int array[length])
     return 0;
 }
 
+int find_odd_test(size_t length, const int array[length])
+{
+    int odd_int = 0;
+
+    for (size_t i = 0; i < length; i++)
+    {
+        odd_int ^= array[i];
+        printf("odd_int : %d\n", odd_int);
+    }
+
+    return odd_int;
+}
+
 void main()
 {
     printf("main starts HERE \n");
 
-    int num;
-    printf("type a number to \n");
-    scanf("%d", &num);
+    const int arr[] = {5, 5, 1, 1, 3};
 
-    printf("result is %d yeeee \n", solution(num));
+    printf("result is %d yeeee \n", find_odd_test(5, arr));
     // printf("result is %d yeeee \n", num);
     printf("main finish HERE \n");
 }
